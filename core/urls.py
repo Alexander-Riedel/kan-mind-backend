e4be77c605eb3308cb11707e38744840bae90dc2
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-06-08 19:08:55 +0200

    add comments in core urls

--- core/urls.py
+++ core/urls.py
@@ -1,24 +1,15 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin interface – accessible via /admin/
     path('admin/', admin.site.urls),
+
+    # Authentication-related API endpoints (registration, login, etc.)
+    # These are defined in auth_app/api/urls.py
     path('api/', include('auth_app.api.urls')),
+
+    # Kanban board-related API endpoints (boards, tasks, comments, etc.)
+    # These are defined in kanban_app/api/urls.py
     path('api/', include('kanban_app.api.urls')),
 ]
